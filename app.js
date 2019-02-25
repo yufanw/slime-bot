397cf73665c72be4c8648801794a0482b4fb5d30
robinsongz <mrrobinsung@gmail.com>
2019-02-25 12:55:25 -0800

    revert back to old code

--- app.js
+++ app.js
@@ -86,62 +86,6 @@ bot.on('ready', () => {
         enmap.ensure(guild.id, defaultSettings);
         let banquetTime = enmap.get(guild.id, 'banquetTime');
             
-             // NA CRONJOBS //
-            
-            // expedition reminder
-            
-            cron.schedule('00 45 11,19 * * *', () => {
-            
-            
-                    let expoChannel = enmap.get(guild.id, "expoChannel");
-                        
-                    let expoMessage = enmap.get(guild.id, "expoMessage");
-            
-                        
-                    guild.channels
-                        .find((channel) => {
-                            if (channel.name === expoChannel) {
-                                channel
-                                    .send(expoMessage)
-                                    .catch(console.error);
-                            } else {
-                                return;
-                            }
-                        })
-            
-                });
-
-               
-            // exped auto clear
-            cron.schedule('00 01 13,21 * * *', () => {
-        
-                enmap.ensure(guild.id, defaultSettings);
-        
-                enmap.set(guild.id, [], 'team1.team');
-                enmap.set(guild.id, [], 'team2.team');
-                enmap.set(guild.id, [], 'team3.team');
-            });
-
-             // guild fort reminder
-            cron.schedule('00 45 20 * * *', () => {
-       
-                let fortChannel = enmap.get(guild.id, "fortChannel");
-                
-                let fortMessage = enmap.get(guild.id, "fortMessage");
-        
-                    
-                guild.channels
-                .find((channel) => {
-                    if (channel.name === fortChannel) {
-                        channel
-                            .send(fortMessage)
-                            .catch(console.error);
-                    } else {
-                        return;
-                    }
-                })
-            });
-
          
             // banquet reminder
             cron.schedule(`00 ${banquetTime} * * *`, () => {
@@ -153,14 +97,85 @@ bot.on('ready', () => {
                     .find(channel => channel.name === banquetChannel)
                     .send(banquetMessage)
                     .catch(console.error);
-            });
+            }, {
+                scheduled: true,
+                timezone: "America/Los_Angeles"
+              });
 
           
         })
     })
 
+//expedition reminder
+bot.guilds.forEach((guild => {
+    cron.schedule('00 45 11,19 * * *', () => {
+            
+            
+        let expoChannel = enmap.get(guild.id, "expoChannel");
+            
+        let expoMessage = enmap.get(guild.id, "expoMessage");
 
+            
+        guild.channels
+            .find((channel) => {
+                if (channel.name === expoChannel) {
+                    channel
+                        .send(expoMessage)
+                        .catch(console.error);
+                } else {
+                    return;
+                }
+            })
 
+    }, {
+        scheduled: true,
+        timezone: "America/Los_Angeles"
+      });
+
+}))
+
+//expedition auto clear
+bot.guilds.forEach((guild => {
+      // exped auto clear
+            cron.schedule('00 01 13,21 * * *', () => {
+        
+                enmap.ensure(guild.id, defaultSettings);
+        
+                enmap.set(guild.id, [], 'team1.team');
+                enmap.set(guild.id, [], 'team2.team');
+                enmap.set(guild.id, [], 'team3.team');
+            }, {
+                scheduled: true,
+                timezone: "America/Los_Angeles"
+              });
+
+}))
+
+//fort reminder
+bot.guilds.forEach((guild => {
+     // guild fort reminder
+     cron.schedule('00 45 20 * * *', () => {
+       
+        let fortChannel = enmap.get(guild.id, "fortChannel");
+        
+        let fortMessage = enmap.get(guild.id, "fortMessage");
+
+            
+        guild.channels
+        .find((channel) => {
+            if (channel.name === fortChannel) {
+                channel
+                    .send(fortMessage)
+                    .catch(console.error);
+            } else {
+                return;
+            }
+        })
+    }, {
+        scheduled: true,
+        timezone: "America/Los_Angeles"
+      });
+}))
 
 // logs unhandled rejections
 process.on('unhandledRejection', (reason, promise) => {
